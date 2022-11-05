d0aad3bf0e54f9f49df4801f0cb3b5e012308990
linhhd1kt <haduclinhbkit@gmail.com>
2022-11-05 12:07:32 +0700

    update rick table view

--- TheMovieDBSwift/Presentation/Base/RickTableView.swift
+++ TheMovieDBSwift/Presentation/Base/RickTableView.swift
@@ -6,8 +6,24 @@
 //
 
 import UIKit
+import RxSwift
+
+protocol RickTableViewType {
+    var input: RickTableViewInputType { get }
+    var output: RickTableViewOutputType { get }
+}
+
+protocol RickTableViewInputType {
+    var startFetch: AnyObserver<Void> { get }
+}
+protocol RickTableViewOutputType {
+}
 
 class RickTableView: UITableView {
+    // FIXME: use service facade to inject viewmodel here: https://somethinglink.info
+    private let viewModel: RickTableViewModelType = RickTableViewModel()
+    private let startFetchObserver = PublishSubject<Void>()
+    private let resultObserver = PublishSubject<[Movie]>()
     
     required init?(coder: NSCoder) {
         super.init(coder: coder)
@@ -22,5 +38,46 @@ class RickTableView: UITableView {
     private func configure() {
         refreshControl = UIRefreshControl()
         refreshControl?.tintColor = R.color.secondary()
+        rx.setDelegate(self).disposed(by: disposeBag)
+        register(MovieTableCell.self)
+        rowHeight = 40
+        separatorStyle = .none
+        bindInput(viewModel.input)
+        bindOutput(viewModel.output)
+    }
+    
+    private func bindInput(_ input: RickTableViewModelInputType) {
+        startFetchObserver
+            .debug("XXX starFeth")
+            .bind(to: input.startFetch)
+            .disposed(by: disposeBag)
+    }
+    private func bindOutput(_ output: RickTableViewModelOutputType) {
+        output.fetchResult
+            .debug("XXX fetchResult")
+            .bind(to: rx.items(cellIdentifier: MovieTableCell.className, cellType: MovieTableCell.self)) { _, model, cell in
+                cell.configure(model)
+            }
+            .disposed(by: disposeBag)
     }
 }
+
+extension RickTableView: RickTableViewInputType {
+    var startFetch: AnyObserver<Void> {
+        return startFetchObserver.asObserver()
+    }
+}
+extension RickTableView: RickTableViewOutputType { }
+
+extension RickTableView: RickTableViewType {
+    var input: RickTableViewInputType {
+        return self
+    }
+    var output: RickTableViewOutputType {
+        return self
+    }
+}
+
+extension RickTableView: HasDisposeBag { }
+
+extension RickTableView: UIScrollViewDelegate { }
