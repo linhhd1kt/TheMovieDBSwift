7ec3bcfb68518778623e823d2c300e062c74c5ae
linhhd1kt <haduclinhbkit@gmail.com>
2022-11-21 09:23:59 +0700

    observer rx resource to view memory count

--- TheMovieDBSwift/Application/AppDelegate.swift
+++ TheMovieDBSwift/Application/AppDelegate.swift
@@ -6,12 +6,19 @@
 //
 
 import UIKit
+import RxSwift
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         ServiceFacade.registerDefaultService()
+        #if DEBUG
+        _ = Observable<Int>.interval(.seconds(1), scheduler: MainScheduler.instance)
+            .subscribe(onNext: { _ in
+                print("Rx Resource Count: \(Resources.total)")
+            })
+        #endif
         return true
     }
     
